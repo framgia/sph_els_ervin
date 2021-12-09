4922e234bb2b27bf6670e9b1187d03f781696a3f
ErvinJMesa <ervinjeremiah.mesa@sun-asterisk.com>
2021-12-09 21:09:18 +0800

    Create User Model and Migrations

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar');
+            $table->tinyInteger('is_admin');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
