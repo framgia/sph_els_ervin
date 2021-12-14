630d102b5b863da81bef42b0a54a13cd5ff34c57
ErvinJMesa <ervinjeremiah.mesa@sun-asterisk.com>
2021-12-14 23:43:42 +0800

    User Authentication - Create Register Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,15 @@ import './App.css';
 import Navbar from './layout/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import SplashPage from './pages/SplashPage';
+import RegistrationPage from './pages/RegistrationPage';
+
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Routes>
         <Route path='/' element={<SplashPage />} />
+        <Route path='/register' element={<RegistrationPage />} />
       </Routes>
     </div>
   );
