e6b97cd3d34e9a461ff9beae86daa2ad6b33edbd
ErvinJMesa <ervinjeremiah.mesa@sun-asterisk.com>
2021-12-14 23:43:42 +0800

    User Authentication - Create Register Frontend

---
+++ frontend/src/pages/RegistrationPage.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+// enum GenderEnum {
+//   female = 'female',
+//   male = 'male',
+//   other = 'other',
+// }
+
+// interface IFormInput {
+//   firstName: String;
+//   gender: GenderEnum;
+// }
+
+interface IFormInput {
+  email: String;
+  name: String;
+  password: String;
+}
+
+export default function RegistrationPage() {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  return (
+    <div className='container mx-auto w-1/4 mt-5'>
+      <div className='p-10 card bg-base-200'>
+        <form onSubmit={handleSubmit(onSubmit)} className='form-control'>
+          <label className='label'>Email</label>
+          <input className='input' {...register('email')} />
+          <label className='label'>Name</label>
+          <input className='input' {...register('name')} />
+          <label className='label'>Password</label>
+          <input type='password' className='input' {...register('password')} />
+          <input
+            className='btn btn-info mt-5 w-1/2 mx-auto'
+            type='submit'
+            value='Register'
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
