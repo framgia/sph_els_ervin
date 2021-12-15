f964b6a73578bf5332be6290fe30165fb12a4e7e
ErvinJMesa <ervinjeremiah.mesa@sun-asterisk.com>
2021-12-15 16:59:59 +0800

    [SELS-TASK] User Authentication - Create Login Page

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+// enum GenderEnum {
+//   female = 'female',
+//   male = 'male',
+//   other = 'other',
+// }
+
+// interface IFormInput {
+//   firstName: String;
+//   gender: GenderEnum;
+// }
+
+interface IFormInput {
+  email: String;
+  password: String;
+}
+
+export default function LoginPage() {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  return (
+    <div className='container mx-auto w-1/4 mt-5'>
+      <div className='p-10 card bg-base-200'>
+        <form onSubmit={handleSubmit(onSubmit)} className='form-control'>
+          <label className='label'>Email</label>
+          <input className='input' {...register('email')} />
+          <label className='label'>Password</label>
+          <input type='password' className='input' {...register('password')} />
+          <input
+            className='btn btn-info mt-5 w-1/2 mx-auto'
+            type='submit'
+            value='Login'
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
