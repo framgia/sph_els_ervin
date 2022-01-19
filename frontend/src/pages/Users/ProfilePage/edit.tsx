b48f37c0fa85c6253f197fe2c7102bfa794b04d7
ErvinJMesa <95011977+ErvinJMesa@users.noreply.github.com>
2022-01-19 17:53:11 +0800

    [SELS-TASK] User Status - Changing of Password (#34)
    * [SELS-TASK] User Status - Changing of Password
    
    * Code Readability and UX for profile editing

--- frontend/src/pages/Users/ProfilePage/edit.tsx
+++ frontend/src/pages/Users/ProfilePage/edit.tsx
@@ -7,6 +7,7 @@ import { config } from '../../../actions/config';
 import FormError from '../../../components/FormError';
 import Loading from '../../../components/Loading';
 import { useNavigate } from 'react-router-dom';
+import API from '../../../api/baseAPI';
 
 interface Props {
   currentLogin?: SessionData;
@@ -16,6 +17,8 @@ interface ProfileFormData {
   avatar: FileList;
   email: string;
   name: string;
+  old_password?: string;
+  new_password?: string;
 }
 
 function EditProfilePage({ currentLogin }: Props): ReactElement {
@@ -23,6 +26,7 @@ function EditProfilePage({ currentLogin }: Props): ReactElement {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors, dirtyFields },
   } = useForm<ProfileFormData>({
     defaultValues: {
@@ -31,6 +35,7 @@ function EditProfilePage({ currentLogin }: Props): ReactElement {
       avatar: undefined,
     },
   });
+  const watchOldPasswordField = watch('old_password');
   const navigate = useNavigate();
 
   const profileDataValidation = {
@@ -49,6 +54,9 @@ function EditProfilePage({ currentLogin }: Props): ReactElement {
           ) || 'Invalid File Type',
       },
     },
+    password: {
+      minLength: 8,
+    },
   };
 
   // Provided isDirty function from react hooks form is buggy
@@ -56,11 +64,19 @@ function EditProfilePage({ currentLogin }: Props): ReactElement {
     return (
       dirtyFields.avatar !== undefined ||
       dirtyFields.email !== undefined ||
-      dirtyFields.name !== undefined
+      dirtyFields.name !== undefined ||
+      dirtyFields.old_password !== undefined ||
+      dirtyFields.new_password !== undefined
     );
   };
 
-  const onSubmit = ({ avatar, name, email }: ProfileFormData) => {
+  const onSubmit = ({
+    avatar,
+    name,
+    email,
+    old_password,
+    new_password,
+  }: ProfileFormData) => {
     if (!isDirty()) return;
     if (!currentLogin) return;
     const newAvatar = avatar[0];
@@ -70,23 +86,20 @@ function EditProfilePage({ currentLogin }: Props): ReactElement {
     if (dirtyFields.avatar) formData.append('avatar', newAvatar);
     formData.append('email', email);
     formData.append('name', name);
+    if (old_password && new_password) {
+      formData.append('old_password', old_password);
+      formData.append('new_password', new_password);
+    }
     setLoading(true);
-    axios
-      .post(`${config.URL}/users/${currentLogin.user.id}`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          Authorization: `Bearer ${currentLogin.token}`,
-        },
-      })
-      .then((res) => {
-        currentLogin.user = res.data;
-        navigate('../');
-      });
+    API.post(`/users/${currentLogin.user.id}`, formData).then((res) => {
+      currentLogin.user = res.data;
+      navigate('../');
+    });
   };
 
   return (
     <div className='container mx-auto w-1/2 mt-5'>
-      <div className='p-10 card bg-base-200'>
+      <div className='p-10 card bg-base-200 transition-all ease-in-out'>
         <form onSubmit={handleSubmit(onSubmit)} className='form-control'>
           <label className='label'>Email</label>
           <input
@@ -100,6 +113,28 @@ function EditProfilePage({ currentLogin }: Props): ReactElement {
             {...register('name', profileDataValidation.name)}
           />
           <FormError message={errors.name?.message} />
+
+          <label className='label'>Old Password</label>
+          <input
+            className='input'
+            type='password'
+            {...register('old_password', profileDataValidation.password)}
+          />
+          <FormError message={errors.old_password?.message} />
+
+          <label
+            className={`label ${
+              !watchOldPasswordField && 'hidden'
+            } transition-all ease-in-out`}
+          >
+            New Password
+          </label>
+          <input
+            className={`input ${!watchOldPasswordField && 'hidden'}`}
+            type='password'
+            {...register('new_password', profileDataValidation.password)}
+          />
+          <FormError message={errors.new_password?.message} />
           <label className='label'>Avatar</label>
           <input
             type='file'
@@ -116,7 +151,7 @@ function EditProfilePage({ currentLogin }: Props): ReactElement {
               'Edit Profile'
             )}
           </button>
-          {loading ? <Loading /> : ''}
+          {loading && <Loading />}
         </form>
       </div>
     </div>
